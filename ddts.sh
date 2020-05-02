61f4b538cde9be0456ee525b49326fad3b8f609a
Soma R Edunuri <soma@cisco.com>
2020-05-01 23:38:23 -0700

    add pims directory

---
+++ ddts.sh
@@ -0,0 +1,4 @@
+pims ri -from EFR-$2 -to EFR-$3 -lineup $1 -ddts -group EFR | grep CSC | sort -u
+
+
+
---
+++ pims/pims_ddts.sh
@@ -0,0 +1,2 @@
+pims ri -from EFR-$1 -to EFR-$2 -lineup $3 -ddts -group EFR | grep CSC | sort -u
+pims ri -from EFR-$1 -to EFR-$2 -lineup $3 -ddts -group EFR 
